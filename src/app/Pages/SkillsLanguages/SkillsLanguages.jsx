65cce808adbd145ecbe42cd415e1641472250667
Mariam Martirosyan <76228722+MariamMartirosyann@users.noreply.github.com>
2022-09-07 21:44:25 +0400

    fix: skills-languages page

--- src/app/Pages/SkillsLanguages/SkillsLanguages.jsx
+++ src/app/Pages/SkillsLanguages/SkillsLanguages.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Sidebar from "../../Shared/Sidebar/Sidebar";
 import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
 import { useSelector, useDispatch } from "react-redux";
+import { useForm, FormProvider } from "react-hook-form";
 import { useMediaQuery } from "react-responsive";
 import { addSkills, deleteSkills } from "../../Redux/SkillsSlice";
 import { preWrittenTextState } from "../../Redux/PreWrittenSlice";
-import { FormProvider } from "react-hook-form";
 import { Typography, Box, Grid } from "@mui/material";
 import { ReactComponent as Close } from "../../../icons/close.svg";
 import { ReactComponent as Add } from "../../../icons/add.svg";
 import { ReactComponent as Edit } from "../../../icons/edit.svg";
+import InputSubmit from "../../Shared/InputSubmit";
 import Input from "../../Shared/Input";
 import { nanoid } from "@reduxjs/toolkit";
 import PreWritten from "../PreWritten";
 import "../../../App.css";
 import "./style.css";
-import InputSubmit from "../../Shared/InputSubmit";
+
 
 const SkillsLanguages = () => {
   const isMediumScreen = useMediaQuery({ query: "(max-width: 1100px)" });
@@ -37,6 +37,7 @@ const SkillsLanguages = () => {
 
   const showPrewritten = () => {
     dispatch(preWrittenTextState(true));
+    console.log("hiiiiiiiiiiiiiiiiiiiii")
   };
   const handleAdd = () => {
     navigate("/languages/add");
@@ -83,12 +84,12 @@ const SkillsLanguages = () => {
             </div>
             <Box className="marginTopBottom30">
               <Grid container>
-                <Grid item lg={3} xs={3}>
-                  <Typography variant="h6" className="marginTopBottom30">
+                <Grid item lg={4} xs={4}>
+                  <Typography variant="h6" className="marginTopm30">
                     Experience
                   </Typography>
                 </Grid>
-                <Grid item g={3} xs={3}>
+                <Grid item lg={4} xs={4}>
                   <Box className="showPrewritten">
                     <Typography variant="p" onClick={showPrewritten}>
                       <Add /> Add pre-written text
@@ -132,14 +133,13 @@ const SkillsLanguages = () => {
                 </Box>
               </form>
             </FormProvider>
-            <br />
-            <br />
+            
             <div className="marginTB3015">
               <Typography variant="h5" className="titleBig">
                 Speak multiple languages?
               </Typography>
             </div>
-            <div className={isMediumScreen ? "smallTitleSmall" : "smallTitle"}>
+            <div className={isMediumScreen ? "smallTitleSmall" : "smallTitle1"}>
               <Typography variant="p">
                 Add your languages and levels of ability here (only if you speak
                 more than one language).
@@ -185,7 +185,7 @@ const SkillsLanguages = () => {
                 Almost finished the basics! Just add a list of skills
               </Typography>
             </div>
-            <div className={isMediumScreen ? "smallTitleSmall" : "smallTitle"}>
+            <div className={isMediumScreen ? "smallTitleSmall" : "smallTitle1"}>
               <Typography variant="p">
                 The skills you add should reflect the requirements of the job
                 you're applying for.
@@ -197,21 +197,12 @@ const SkillsLanguages = () => {
                   Experience
                 </Typography>
               </Grid>
-              <Grid item xs={4}>
-                {" "}
-                <Typography
-                  variant="h6"
-                  onClick={showPrewritten}
-                  style={{
-                    marginLeft: "0",
-                    marginTop: "30px",
-                    marginBottom: "30px",
-                    color: "rgb(103, 103, 241)",
-                    textAlign: "end",
-                  }}
-                >
-                  <Add /> Add pre-written text
-                </Typography>
+              <Grid item lg={6} xs={8}>
+              <Box className="showPrewritten">
+                    <Typography variant="p" onClick={showPrewritten}>
+                      <Add /> Add pre-written text
+                    </Typography>
+                  </Box>
               </Grid>
             </Grid>
 
@@ -256,41 +247,21 @@ const SkillsLanguages = () => {
                 </Box>
               </form>
             </FormProvider>
-            <br />
-            <br />
-            <Typography
-              variant="h3"
-              style={{
-                marginLeft: "0",
-                marginTop: "30px",
-                marginBottom: "15px",
-              }}
-            >
-              Speak multiple languages?
-            </Typography>
-            <Typography
-              variant="p"
-              style={{
-                width: "200px",
-                marginLeft: "0",
-                marginTop: "10px",
-                marginBottom: "35px",
-              }}
-            >
-              Add your languages and levels of ability here (only if you speak
-              more than one language).
-            </Typography>
-            <Typography
-              variant="h6"
-              style={{
-                marginLeft: "0",
-                marginTop: "30px",
-                marginBottom: "30px",
-              }}
-            >
+           
+            <div className="marginTB3015">
+              <Typography variant="h5" className="titleBig">
+                Speak multiple languages?
+              </Typography>
+            </div>
+            <div className={isMediumScreen ? "smallTitleSmall" : "smallTitle1"}>
+              <Typography variant="p">
+                Add your languages and levels of ability here (only if you speak
+                more than one language).
+              </Typography>
+            </div>
+            <Typography variant="h6" className="marginTop30">
               Languages
             </Typography>
-
             {langList.map((i) => (
               <Grid className="container" key={i.id}>
                 <Grid className="item1">
