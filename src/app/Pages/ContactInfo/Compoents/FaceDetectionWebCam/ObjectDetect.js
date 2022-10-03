7fedc4a87091b3dc9597ba1a4140b5cfe94e66fb
Mariam Martirosyan <76228722+MariamMartirosyann@users.noreply.github.com>
2022-10-03 23:32:01 +0400

    fix: take a pickture

--- src/app/Pages/ContactInfo/Compoents/FaceDetectionWebCam/ObjectDetect.js
+++ src/app/Pages/ContactInfo/Compoents/FaceDetectionWebCam/ObjectDetect.js
@@ -1,5 +1,6 @@
 // Import dependencies
 import React, { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import * as tf from "@tensorflow/tfjs";
 // 1. TODO - Import required model here
 // e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
@@ -7,17 +8,34 @@ import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./style.css";
 // 2. TODO - Import drawing utility here
- import { drawRect } from "./utilities";
+import { drawRect } from "./utilities";
+import { Navigate } from "react-router";
+
+
+const option = {
+  width: 400,
+  height: 400,
+  facingMode: "user",
+};
 
 function Detection() {
+
+  const navigate = useNavigate();
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  const photoRef = useRef(null)
+
+  const [img, setImg] = useState("");
+
+  const takeImg = React.useCallback(() => {
+    const imgSrc = webcamRef.current.getScreenshot();
+    setImg(imgSrc);
+  });
+
   // Main function
   const runCoco = async () => {
     // 3. TODO - Load network 
     // e.g. const net = await cocossd.load();
-    const net= await cocossd.load();
+    const net = await cocossd.load();
     //  Loop and detect hands
     setInterval(() => {
       detect(net);
@@ -45,8 +63,8 @@ function Detection() {
       canvasRef.current.height = videoHeight;
 
       // 4. TODO - Make Detections
-       const obj = await net.detect(video);
-       console.log(obj)
+      const obj = await net.detect(video);
+      console.log(obj)
 
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
@@ -57,31 +75,15 @@ function Detection() {
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(() => { runCoco() }, []);
 
-  const takePicture = () => {
-    const width = 400
-    const height = width / (16 / 9)
-    
-   // let video = videoRef.current
- 
-    const photo = photoRef.current
- 
-    photo.width = width
- 
-    photo.height = height
- 
-    let ctx = photo.getContext('2d')
- 
-    ctx.drawImage(video, 0, 0, width, height)
-    
-  }
+
   return (
     <div >
-      <header>
+      {/* <header>
         <Webcam
           ref={webcamRef}
-          muted={true} 
+          muted={true}
           style={{
             position: "absolute",
             marginLeft: "auto",
@@ -109,7 +111,130 @@ function Detection() {
             height: 480,
           }}
         />
-      </header>
+      </header> */}
+
+      <div>
+        <div>
+        <>
+              <Webcam
+                audio={false}
+                ref={webcamRef}
+                muted={true}
+                screenshotFormat="image/jpeg"
+                videoConstraints={option}
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                  zindex: 9,
+                  width: 400,
+                  height: 400,
+                }}
+              />
+
+              <canvas
+                ref={canvasRef}
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                  zindex: 8,
+                  width: 400,
+                  height: 400,
+                }}
+              />
+            </>
+          {img == "" ? (
+            <>
+              <Webcam
+                audio={false}
+                ref={webcamRef}
+                muted={true}
+                screenshotFormat="image/jpeg"
+                videoConstraints={option}
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                  zindex: 9,
+                  width: 400,
+                  height: 400,
+                }}
+              />
+
+              <canvas
+                ref={canvasRef}
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                  zindex: 8,
+                  width: 400,
+                  height: 400,
+                }}
+              />
+            </>
+          ) : (
+            <img src={img} />
+          )}
+        </div>
+        {/* <button
+              onClick={(e) => {
+                e.preventDefault();
+                takeImg();
+              }}
+              className="btn btn-dark"
+            >
+              Retake
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                takeImg();
+              }}
+              className="btn btn-primary"
+            >
+              Click image
+              </button> */}
+        <div>
+         {img != "" ? (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                takeImg();
+              }}
+              className="btn btn-dark"
+            >
+              Retake
+            </button>
+            
+           
+          ) : (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                takeImg();
+              }}
+              className="btn btn-primary"
+            >
+              Click image
+            </button>
+          )} 
+        </div>
+      </div>4
+
     </div>
   );
 }
